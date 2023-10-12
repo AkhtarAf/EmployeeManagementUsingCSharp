922d31f761b6e13cabf1b5f9bb01a3e1f803af22
Afshan Akhtar <akhtaraf@sheridancollege.ca>
2023-10-12 00:17:48 -0400

    Add project files.

---
+++ A1AfshanAkhtar/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace A1AfshanAkhtar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employee = new List<Employee>();
+            employee.Add(new Employee(SalariedEmployee, 0, "Afshan", "Sales"));
+        }
+    }
+}
